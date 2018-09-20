f21ff10dde750260ecd6f49763b6d5d76046bd80
nasrsyncbac <nasr.mohammed@syncbak.com>
2018-09-20 16:27:53 -0500

    added some changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World Nasr Aldden Osman Oshar Mohammed, who is makes oil boil :)!');
+  res.send('Hello World Nasr Aldden Osman Oshar Mohammed, who is makes oil boil!');
 });
 
 var server = app.listen(3000, function () {
