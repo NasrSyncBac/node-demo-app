b4a0f94b4894f6a451eee785c844621a69164245
nasrsyncbac <nasr.mohammed@syncbak.com>
2018-09-20 15:27:36 -0500

    added some changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World Nasr Aldden Osman Oshar Mohammed!');
+  res.send('Hello World Nasr Aldden Osman Oshar Mohammed :)!');
 });
 
 var server = app.listen(3000, function () {
